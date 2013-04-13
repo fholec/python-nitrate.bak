9d638d8c91c3c319474a5efaaf65dc9dfccdea99
Filip Holec <fholec@redhat.com>
2013-04-13 23:18:06 +0200

    Updated TCMS init script, added .nitrate for illustration

--- init_tcms.py
+++ init_tcms.py
@@ -8,6 +8,10 @@ from os import system
 
 TAGS = {0:"Apps", 1:"Tier 1", 2:"new", 3:"Tier 2", 4:"TIP"}
 
+# Import test data from .nitrate configuration file
+# ! .nitrate has to contain [init] section !
+initconfig = nitrate.Nitrate()._config.init
+
 def addbug(id):
     return [id, random.randint(1,10000), 1, "Summary", "Description"]
 
@@ -36,32 +40,32 @@ if __name__ == "__main__":
 
     # Create master plan (root)
     master = nitrate.TestPlan(name="Master Test Plan 2", \
-        product="RHEL Tests", version="unspecified", \
-        type=nitrate.PlanType(26))
+        product=config.product, version="unspecified", \
+        type=nitrate.PlanType(config.plantype))
     master.update()
 
     for i in range(0, options.plans):
         # Create Plans
         tplan = nitrate.TestPlan(name="Test Plan {0}".format(i+1),\
-            parent=master.id, product="RHEL Tests", version="unspecified",\
-            type=nitrate.PlanType(26))
+            parent=master.id, product=config.product, version="unspecified",\
+            type=nitrate.PlanType(config.plantype))
         tplan.update()
         for c in range(0, options.cases):
             # Create cases
             case = nitrate.TestCase(name="Test Case {0}".format(i+1),\
-                    category=nitrate.Category(702), product="RHEL Tests",\
+                    category=nitrate.Category(config.category), product=config.product,\
                     summary="Test Case {0}".format(i+1),\
-                    status=nitrate.CaseStatus(2))
+                    status=nitrate.CaseStatus(config.casestatus))
             case.update()
             # Link with TestPlan
             nitrate.TestCase(case.id).testplans._add([nitrate.TestPlan(tplan.id)])
             # Add a tag
             nitrate.TestCase(case.id).tags._add([TAGS[random.randint(0,4)]])
             # Add bug
-            nitrate.TestCase(case.id).bugs.add(nitrate.Bug(1000))
+            nitrate.TestCase(case.id).bugs.add(nitrate.Bug(config.bug))
         for r in range(0, options.runs):
             # Create runs
             run = nitrate.TestRun(name="Test Run {0}".format(i+1),\
-                    testplan=nitrate.TestPlan(tplan.id), build=nitrate.Build(979),\
-                    product="RHEL Tests", summary="Test Run",\
-                    version=nitrate.Version(389).name)
+                    testplan=nitrate.TestPlan(tplan.id), build=nitrate.Build(config.build),\
+                    product=config.product, summary="Test Run",\
+                    version=nitrate.Version(config.version).name)
